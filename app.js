07f68b4e392c8e0e5261700dc00e2c849af15669
ruhulsinbad <ruhulsinbad@gmail.com>
2021-12-13 20:55:40 +0600

    Register Login Works

--- app.js
+++ app.js
@@ -2,10 +2,22 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const port = process.env.PORT || 4000;
+
+const dbURL = "mongodb+srv://sinbad:1561Sinbad3389@cluster0.q94ur.mongodb.net/testPerson?retryWrites=true&w=majority";
+
+mongoose.connect(dbURL);
+
+const personSchema = {
+    email: String,
+    password: String
+}
+
+const Person = new mongoose.model("Person", personSchema);
 
 
 app.set('view engine', 'ejs');
@@ -27,6 +39,49 @@ app.get('/login', (req, res) =>{
 app.get('/register', (req, res) =>{
     res.render("register")
 })
+
+
+app.post('/register', (req, res) =>{
+    const newPerson = new Person({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newPerson.save((err)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("secrets");
+        }
+    });
+    
+})
+
+app.post('/login', (req, res) =>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    Person.findOne({email: username}, (err, foundPerson)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(foundPerson){
+                if(foundPerson.password === password){
+                    res.render("secrets");
+                }
+                else{
+                    res.write("Incorrect password");
+                    res.send();
+                }
+            }
+        }
+    })
+})
+
+
+
 
 app.listen(port, ()=>{
     console.log("Server is listening on port " + port);
